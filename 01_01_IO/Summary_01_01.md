f31985dd10c5e9612b4ee116270d75acada9ab84
joonleehello <leecpkn@naver.com>
2026-09-21 16:13:38 +0900

    Start FunctionOverloading Chapter

--- 01_01_IO/Summary_01_01.md
+++ 01_01_IO/Summary_01_01.md
@@ -1,4 +1,4 @@
-# Chapter 01 - C언어 기반의 C++
+# Chapter 01_01 - C++에서의 입출력
 
 ## HelloWorld.cpp
 
---
+++ 01_02_FunctionOverloading/FunctionOverloading.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+
+void MyFunc(void) {
+	std::cout << "MyFunc(void) called" << std::endl;
+}
+
+void MyFunc(int a) {
+	std::cout << "MyFunc(int a) called" << std::endl;
+}
+
+void MyFunc(int a, int b) {
+	std::cout << "MyFunc(int a, int b) called" << std::endl;
+}
+
+int main(void) {
+	MyFunc();
+	MyFunc(20);
+	MyFunc(15, 20);
+
+	return 0;
+}
